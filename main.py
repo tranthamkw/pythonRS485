640fd0e5c16609cc6430dc4e945c308b21868cd0
Kenneth Trantham <tranthamkw@unk.edu>
2025-03-24 21:58:58 -0500

    minor changes

--- main.py
+++ main.py
@@ -8,22 +8,32 @@ import re
 import os
 #import threading
 #from datetime import datetime
-
+import KeithleyInstruments
 #import usbRS485bridge
 import port
 #import globalVars
+import rs485Devices
 
+K485GPIB=10
+K485RS485=0xC3
 
 # this is a sand pit to test various things before wrapping into a dedicated main script#
 # ++++++++++++++++++++	START MAIN +++++++++++++++++++++++#
 #
-
-
-myports=port.getallportsastext()
-
-for i in range(len(myports)):
-	print(myports[i])
+z=0.0
 
 
 
+rs485Devices.init()
+
+
+KeithleyInstruments.iniK485(K485RS485,K485GPIB)
+
+z=KeithleyInstruments.readK485(K485RS485,K485GPIB)
+
+print("ammeter reading {}".format(z))
+
+print("OK")
+
+rs485Devices.stop()
 os._exit(0)
