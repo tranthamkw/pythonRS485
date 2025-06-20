679a874da9506355c57df46a11070995daa6ebe3
Kenneth Trantham <tranthamkw@unk.edu>
2025-06-20 09:34:12 -0500

    minor

--- battboxtest.py
+++ battboxtest.py
@@ -17,38 +17,37 @@ SERVODEVICE=0xD3
 
 
 parser = argparse.ArgumentParser(
-        prog='scanSEE',
-        description='Scans KEPCO power supply from start to stop potential',
-        epilog="e.g. python scanSEE.py <start v> <end v> <step v>")
-parser.add_argument('startv', type=int, help='start volts')
-
+        prog='battboxtest',
+        description='sets battbox to one of nine possible outputs ',
+        epilog="e.g. python battboxtest.py <idx>")
+parser.add_argument('idx', type=int, help='index. possible values 0 through 9')
 args = parser.parse_args()
-x = args.startv
+x = args.idx
 
-
-z=0
 
 interface.rs485Devices.init()
 
-for k in range(5):
+z=interface.rs485Devices.setRS485Battery(DIGIDEVICE,x)
+"""
+for k in range(2):
 	for j in range(9):
 		z=interface.rs485Devices.setRS485Battery(DIGIDEVICE,j)
 		sys.stdout.write("{} ".format(j))
 		sys.stdout.flush()
-#		time.sleep(0.1)
+		time.sleep(0.5)
 	sys.stdout.write("\n")
 
 	for j in range(8,-1,-1):
 		z=interface.rs485Devices.setRS485Battery(DIGIDEVICE,j)
 		sys.stdout.write("{} ".format(j))
 		sys.stdout.flush()
-#		time.sleep(0.1)
+		time.sleep(0.5)
 	sys.stdout.write("\n")
 
 time.sleep(0.1)
 
 z=interface.rs485Devices.setRS485Battery(DIGIDEVICE,0)
-
+"""
 print("OK")
 
 interface.rs485Devices.stop()
